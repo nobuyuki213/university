f5c73a490fbce1ec167b6a29c8ac41cd98e1c4f3
nobuyuki213 <nobu0213yuki@gimail.com>
2018-09-01 23:14:11 +0900

    UP:Breadcrumbs class Not Foundエラーに中止

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ ($breadcrumb = Breadcrumbs::current()) ? $breadcrumb->title.' | ' : '' }}{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
         <!--drawer Css -->
         <link href="{{ asset('css/drawer.min.css') }}" rel="stylesheet">
         <!--hover Css -->
